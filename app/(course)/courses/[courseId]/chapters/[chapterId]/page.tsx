22836636b3448cf45e7a6ce9b9c5c433ee2188d2
jerwintuchi <jerwin.tuchi3@gmail.com>
2024-07-08 02:14:45 +0800

    ui: video player won't render if not enrolled

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -97,7 +97,7 @@ const ChapterIdPage = async ({
       <div className="flex flex-col max-w-4xl mx-auto pb-20">
         <div className="p-4">
           {
-            isEnrolled ? (  
+            isEnrolled && !isLocked ? (  
               <VideoPlayer
               chapterId={params.chapterId}
               title={chapter.title}
