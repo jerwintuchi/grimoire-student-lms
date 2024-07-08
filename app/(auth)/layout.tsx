b189ce469cb0049d113472060b0abe1613ce8d87
jerwintuchi <jerwin.tuchi3@gmail.com>
2024-07-08 19:09:50 +0800

    ui: added bg on landing and auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,9 +4,20 @@ import MainNav from "../landing/mainnav";
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
+      <div className="bg-[#13111c]">
+        <video
+          src={
+            "https://utfs.io/f/5a452323-9047-417a-bf4a-eba2e44de6aa-qe7sdm.mp4"
+          }
+          autoPlay
+          loop
+          muted
+          className="absolute inset-0 w-full h-full object-cover z-0" // Adjust z-index as need
+        />
       <MainNav />
-      <div className="h-[calc(100vh-40px)] flex items-center justify-center bg-white no-scroll">
+      <div className="h-[calc(100vh-72px)] flex items-center justify-center bg-white no-scroll z-20">
         {children}
+      </div>
       </div>
     </>
   );
--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -4,11 +4,22 @@ import Image from "next/image";
 
 const LandingPage = () => {
   return (
-    <div>
-      {/* Navbar */}
-      <MainNav />
-      {/* Landing Page Content */}
-      <div className="bg-white h-[calc(100vh-72px)] text-white bg-full flex flex-col justify-center items-center">
+  <>
+  <div className="relative h-screen">
+      <video
+        src={
+          "https://utfs.io/f/d8a27213-f787-464b-ab88-0adad6026201-sav6v1.mp4"
+        }
+        autoPlay
+        loop
+        muted
+        className="absolute inset-0 w-full h-full object-cover z-0" // comment for prod
+      />
+      <div className="z-10">
+       <MainNav /> 
+      </div>
+      
+      <div className="absolute inset-0 h-[calc(100vh-72px)] text-white bg-full flex flex-col justify-center items-center z-20">
         <Image
           width={50}
           height={50}
@@ -19,6 +30,8 @@ const LandingPage = () => {
         <h1 className="text-4xl">Welcome to Our Website!</h1>
       </div>
     </div>
+  </>
+    
   );
 };
 
